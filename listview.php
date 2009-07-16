310f4b0ecdf6d3ba89cd35ea2288935a09db8114
toby.anderson <toby.anderson@aa218b52-6c23-11de-927a-590d86faf14d>
2009-07-16 01:12:26 +0000

    Can now delete files.

--- listview.php
+++ listview.php
@@ -6,16 +6,38 @@ require 'environment.php';
 mysql_connect(localhost, $db_user, $db_password);
 @mysql_select_db($database) or die( 'Unable to select database');
 
-$qAllFiles = 'SELECT * FROM Files';
+// check if we've been asked to delete a file
+if ($_POST['action'] == 'delete') {
+    echo "<p>Deleting file from archive<br />\n";
+    echo 'archiver says: ' . exec("7za d -y $archive_path file_$_POST['file_id']", $output, $return_value) . "<br />\n";
+    if ($return_value == 0) {
+        $qDelFile = 'DELETE FROM Files WHERE id = ' . $_POST['file_id'];
+        mysql_query($qDelFile) or die ( 'Query failed: ' . mysql_error() . '<br />' . $qDelFile . "</p>\n");
+        echo "File deleted</p>\n";
+    } else {
+        echo "There was an error deleting the file.</p>\n";
+    }
+}
+
+// show all files
+$qAllFiles = 'SELECT id, filename FROM Files';
 $qAllFilesResult = mysql_query($qAllFiles) or die ( 'Query failed: ' . mysql_error() . '<br />' . $qAllFiles );
 
 $num = mysql_numrows($qAllFilesResult);
-echo "<p>Displaying $num files.</p>\n<table>\n";
+echo "<p>Displaying $num files.</p>\n";
 
+echo "<table>\n";
 for ($row = 0; $row < $num; ++$row) {
-    echo '<tr><td>' . mysql_result($qAllFilesResult, $row, 'filename') . '</td></tr>';
+    echo "<tr>\n";
+    echo '<td>' . mysql_result($qAllFilesResult, $row, 'filename') . "</td>\n";
+    echo '<td><form action="listview.php" method="POST">';
+    echo '<input type="hidden" name="action" value="delete" />';
+    echo '<input type="hidden" name="file_id" value="' . mysql_result($qAllFilesResult, $row, 'id') . '" />';
+    echo '<input type="submit" value="Delete" />';
+    echo "</form></td>\n";
+    echo "</tr>\n";
 }
-echo '</table>';
+echo "</table>\n";
 
 mysql_close();
 ?>
