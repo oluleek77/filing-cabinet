34a4f37b955866d58628d2946550450ea57fd30a
toby.anderson <toby.anderson@aa218b52-6c23-11de-927a-590d86faf14d>
2009-07-20 05:16:04 +0000

    Added the environment file that I forgot to add before. Also, looks like I did some stuff to listview.

---
+++ environment.php
@@ -0,0 +1,8 @@
+<?php
+// Don't edit this file directly.
+// Make a copy of install-example.sh and put your values in that
+$db_user = '<insert database username>';
+$db_password = '<insert database password>';
+$database = 'filingcabinet';
+$archive_path = '<insert archive path and filename>';
+?>
