a5a6bc09d417390f8752b0f9416c7504f8fb9cf9
toby.anderson <toby.anderson@aa218b52-6c23-11de-927a-590d86faf14d>
2009-09-14 12:01:48 +0000

    User cannot delete file without loggin in.

--- trunk/listview.php
+++ trunk/listview.php
@@ -1,23 +1,31 @@
 <?php
 
 require 'environment.php';
+require_once 'access.class.php';
 
 // connect to the database
 mysql_connect(localhost, $db_user, $db_password);
 @mysql_select_db($database) or die( 'Unable to select database');
 
+$user = new flexibleAccess($db_link);
+
 // check if we've been asked to delete a file
 if ($_POST['action'] == 'delete') {
-    echo "<p>Deleting file from archive<br />\n";
-    echo 'archiver says: ' . exec("7za d -y $archive_path file_" . $_POST['file_id'], $output, $return_value) . "<br />\n";
-    if ($return_value == 0) {
-        $qDelFile = 'DELETE FROM Files WHERE id = ' . $_POST['file_id'];
-        mysql_query($qDelFile) or die ( 'Query failed: ' . mysql_error() . '<br />' . $qDelFile . "</p>\n");
-        $qDelLabels = 'DELETE FROM Labels WHERE file_id = ' . $_POST['file_id'];
-        mysql_query($qDelLabels) or die ( 'Query failed: ' . mysql_error() . '<br />' . $qDelLabels . "</p>\n");
-        echo "File deleted</p>\n";
+    // must be logged in to delete files
+    if (!($user->is_loaded() and $user->is_active())) {
+        "<p>You need to login to delete files</p>\n";
     } else {
-        echo "There was an error deleting the file.</p>\n";
+        echo "<p>Deleting file from archive<br />\n";
+        echo 'archiver says: ' . exec("7za d -y $archive_path file_" . $_POST['file_id'], $output, $return_value) . "<br />\n";
+        if ($return_value == 0) {
+            $qDelFile = 'DELETE FROM Files WHERE id = ' . $_POST['file_id'];
+            mysql_query($qDelFile) or die ( 'Query failed: ' . mysql_error() . '<br />' . $qDelFile . "</p>\n");
+            $qDelLabels = 'DELETE FROM Labels WHERE file_id = ' . $_POST['file_id'];
+            mysql_query($qDelLabels) or die ( 'Query failed: ' . mysql_error() . '<br />' . $qDelLabels . "</p>\n");
+            echo "File deleted</p>\n";
+        } else {
+            echo "There was an error deleting the file.</p>\n";
+        }
     }
 }
 
