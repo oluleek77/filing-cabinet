c9891c5ac1aaef7c7e778f50933c2e990e1e9800
toby.anderson <toby.anderson@aa218b52-6c23-11de-927a-590d86faf14d>
2009-12-17 08:30:13 +0000

    Made consistent menu across pages with one file for functions to produce common output.

--- trunk/common.php
+++ trunk/common.php
@@ -8,4 +8,49 @@ function mimeFilename ($mimeType)
     return 'gnome-mime-' . str_replace('/', '-', $mimeType) . '.png';
 }
 
+// generate page head
+function head($title = 'Filing Cabinet')
+{
+    return "
+<html>
+<head>
+    <link rel=\"stylesheet\" type=\"text/css\" href=\"filingcabinet-default.css\" />
+    <title>$title</title>
+</head>
+<body>
+";
+}
+
+// generate page foot
+function foot()
+{
+    return "
+</body>
+</html>
+";
+}
+
+// generate tab menufunction head ($title = 'Filing Cabinet')
+function tabMenu($logged = True, $username = '')
+{
+    if ($logged) {
+        $ref = 'login.php?logout=1';
+        $txt = 'Logout';
+    } else {
+        $ref = 'login.php';
+        $txt = 'Login';
+    }
+    return "
+<div id='tabs'>
+  <ul>
+    <li></li>
+    <li><a href=\"listview.php\"><span>List Files</span></a></li>
+    <li><a href=\"upload_files.php\"><span>Upload</span></a></li>
+    <li><a href=\"$ref\"><span>$txt</span></a></li>
+    <li id=\"username\">$username</li>
+  </ul>
+</div>
+";
+}
+
 ?>
