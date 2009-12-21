a8ee7d760c1e693b81e533b94968c9baca5e23da
toby.anderson <toby.anderson@aa218b52-6c23-11de-927a-590d86faf14d>
2009-12-21 12:40:11 +0000

    Show file labels in file-view.

--- fileview.php
+++ fileview.php
@@ -12,15 +12,15 @@ $user = new flexibleAccess($db_link);
 
 // get all the file data
 $qAllFileData = 'SELECT * FROM Files WHERE id = ' . $_GET['id'];
-$qFileDataResult = mysql_query($qAllFileData);
+$rAllFileData = mysql_query($qAllFileData);
 
 // redirect if file does not exist
-if (!$qFileDataResult or (mysql_numrows($qFileDataResult) != 1)) {
+if (!$rAllFileData or (mysql_numrows($rAllFileData) != 1)) {
     header('Location: listview.php');
 }
 // redirect if user does not have permission to view the file
 // (if the file is not public and they are not the owner of it)
-if (mysql_result($qFileDataResult, 0, 'permissions') == 0 and mysql_result($qFileDataResult, 0, 'owner') != $user->get_property('username')) {
+if (mysql_result($rAllFileData, 0, 'permissions') == 0 and mysql_result($rAllFileData, 0, 'owner') != $user->get_property('username')) {
     header('Location: listview.php');
 }
 echo head();
@@ -31,28 +31,43 @@ if ($user->is_loaded() and $user->is_active())
 } else {
     echo tabMenu(False);
 }
-echo '<h2>'. mysql_result($qFileDataResult, 0, 'filename') . '</h2>';
+echo '<h2><img src="images/mimetypes/32/' . mimeFilename(mysql_result($rAllFileData, 0, 'type')) . '" />' . mysql_result($rAllFileData, 0, 'filename') . '</h2>';
+
+// download and delete buttons
+echo '<div><a href="download.php?id=' . $_GET['id'] . '"><img src="images/download-32.png" alt="Download file" /></a>
+<a href="listview.php?action=delete&file_id=' . $_GET['id'] . '"><img src="images/delete-32.png" alt="Delete file" /></a></div>';
 
 // display file data
-echo '<p>MIME Type: ' . mysql_result($qFileDataResult, 0, 'type') . '<img src="images/mimetypes/32/' . mimeFilename(mysql_result($qFileDataResult, 0, 'type')) . '" /></p>';
-echo '<p>File size: ' . mysql_result($qFileDataResult, 0, 'size') . '</p>';
-echo '<p>Owner: ' . mysql_result($qFileDataResult, 0, 'owner') . '</p>';
+echo '<p>MIME Type: ' . mysql_result($rAllFileData, 0, 'type') . '</p>';
+echo '<p>File size: ' . mysql_result($rAllFileData, 0, 'size') . '</p>';
+echo '<p>Owner: ' . mysql_result($rAllFileData, 0, 'owner') . '</p>';
 
-echo '<div><a href="download.php?id=' . $_GET['id'] . '"><img src="images/download-32.png" alt="Download" /></a>
-<a href="listview.php?action=delete&file_id=' . $_GET['id'] . '"><img src="images/delete-32.png" alt="Delete" /></a></div>';
+// get all labels for this file
+$qFileLabels = 'SELECT label_name FROM Labels WHERE file_id = ' . $_GET['id'];
+$rFileLabels = mysql_query($qFileLabels);
+echo '<div id="filelabels">' . "\n";
+echo "<table>\n";
+while ($row = mysql_fetch_assoc($rFileLabels))
+{
+    echo "<tr>\n";
+    echo '<td>' . $row['label_name'] . "</td><td><img src=\"images/delete-32.png\" width=\"16\" height=\"16\" alt=\"Delete label\" /></td>\n";
+    echo "</tr>\n";
+}
+echo "</table>\n";
+echo "</div>\n";
 
 // find previous file in sequence, if it exists.
 $qPrevFile = 'SELECT id, filename FROM Files WHERE next_file_id = ' . $_GET['id'];
-$qPrevFileResult = mysql_query($qPrevFile);
-if (mysql_num_rows($qPrevFileResult) > 0) {
-    echo 'Previous in sequence: <a href="fileview.php?id=' . mysql_result($qPrevFileResult, 0, 'id') . '">' . mysql_result($qPrevFileResult, 0, 'filename') . '</a>';
+$rPrevFile = mysql_query($qPrevFile);
+if (mysql_num_rows($rPrevFile) > 0) {
+    echo 'Previous in sequence: <a href="fileview.php?id=' . mysql_result($rPrevFile, 0, 'id') . '">' . mysql_result($rPrevFile, 0, 'filename') . '</a>';
 }
 
 // get filename for next file in sequence if there is one.
-if (mysql_result($qFileDataResult, 0, 'next_file_id')) {
-    $qNextFile = 'SELECT id, filename FROM Files WHERE id = ' . mysql_result($qFileDataResult, 0, 'next_file_id');
-    $qNextFileResult = mysql_query($qNextFile);
-    echo 'Next in sequence: <a href="fileview.php?id=' . mysql_result($qNextFileResult, 0, 'id') . '">' . mysql_result($qNextFileResult, 0, 'filename') . '</a>';
+if (mysql_result($rAllFileData, 0, 'next_file_id')) {
+    $qNextFile = 'SELECT id, filename FROM Files WHERE id = ' . mysql_result($rAllFileData, 0, 'next_file_id');
+    $rNextFile = mysql_query($qNextFile);
+    echo 'Next in sequence: <a href="fileview.php?id=' . mysql_result($rNextFile, 0, 'id') . '">' . mysql_result($rNextFile, 0, 'filename') . '</a>';
 }
 
 echo foot();
