e95426c407dd9ecf57827f0089b703ad9c4352d5
toby.anderson <toby.anderson@aa218b52-6c23-11de-927a-590d86faf14d>
2010-04-13 05:37:48 +0000

    put in another mime type image that comes up of .odt files

--- common.php
+++ common.php
@@ -62,4 +62,27 @@ function parseLabels($str)
     }
     return $labels;
 }
+
+/**
+ * Returns a human readable filesize
+ *
+ * @author      wesman20 (php.net)
+ * @author      Jonas John
+ * @version     0.3
+ * @link        http://www.jonasjohn.de/snippets/php/readable-filesize.htm
+ */
+function HumanReadableFilesize($size) {
+ 
+    // Adapted from: http://www.php.net/manual/en/function.filesize.php
+ 
+    $mod = 1024;
+ 
+    $units = explode(' ','B KiB MiB GiB TiB PiB');
+    for ($i = 0; $size > $mod; $i++) {
+        $size /= $mod;
+    }
+ 
+    return round($size, 2) . ' ' . $units[$i];
+}
+
 ?>
