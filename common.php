8a949836fa83c04b08ee3f2f569f9b5616f76576
toby.anderson <toby.anderson@aa218b52-6c23-11de-927a-590d86faf14d>
2010-09-07 01:10:50 +0000

    Added a <!DOCTYPE> tag and used some funky jQuery to make list of common labels hide and show.

--- common.php
+++ common.php
@@ -20,6 +20,8 @@ function head($title = 'Filing Cabinet', $scripts = array())
 function headA($scripts)
 {
     $out = "
+<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\"
+    \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">
 <html>
 <head>
     <link rel=\"stylesheet\" type=\"text/css\" href=\"css/filingcabinet-default.css\" />
