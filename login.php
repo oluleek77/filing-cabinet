56c66a836ca348e8e47423d1a6ccad3692050a82
toby.anderson <toby.anderson@aa218b52-6c23-11de-927a-590d86faf14d>
2010-09-11 05:55:30 +0000

    Brought login onto same page as listview. Login is now Ajax. Still needs more stuff to happen after login. Intend to do the same with upload files.

--- login.php
+++ login.php
@@ -16,13 +16,14 @@ if ( !$user->is_loaded() )
 	//Login stuff:
 	if ( isset($_POST['uname']) && isset($_POST['pwd'])){
 	  if ( !$user->login($_POST['uname'],$_POST['pwd'],$_POST['remember'] )){// we don't have to use addslashes as access.class does the job
-	    echo 'Wrong username and/or password';
+	    echo '<span class="failure">Login Failed</span>';
 	  }else{
 	    //user is now loaded
-	    header('Location: http://'.$_SERVER['HTTP_HOST'].$_SERVER['PHP_SELF']);
+	    echo '<span class="success">Login Succeeded</span>';
+	    //header('Location: http://'.$_SERVER['HTTP_HOST'].$_SERVER['PHP_SELF']);
 	  }
 	}
-	if (!isset($_GET['location'])) $_GET['location'] = urlencode('listview.php');
+	/*if (!isset($_GET['location'])) $_GET['location'] = urlencode('listview.php');
 	echo head();
 	echo tabMenu(False);
 	echo '<h2>Filing Cabinet Login</h2>
@@ -39,21 +40,22 @@ if ( !$user->is_loaded() )
             src="http://www.w3.org/Icons/valid-xhtml10-blue.png"
             alt="Valid XHTML 1.0 Strict" height="31" width="88" /></a>
         </p>';
-	echo foot();
+	echo foot();*/
 }
 else if (!$user->is_active()){
-    echo head();
+    //echo head();
     echo '<p>Your account is not active. When you registered you should have got an email giving you directions
     on how to activate your account. If there is a problem contat the administrator.</p>';
-    echo '<p><a href="'.$_SERVER['PHP_SELF'].'?logout=1">logout</a></p>';
-    echo foot();
+    //echo '<p><a href="'.$_SERVER['PHP_SELF'].'?logout=1">logout</a></p>';
+    ///echo foot();
 } else {
+    echo 'Already logged in!';
     // User is loaded, so send them on to where they want to go.
-    if (isset($_POST['location'])) {
-        header('Location: http://'.$_SERVER['HTTP_HOST'].'/'.$rel_web_path.'/'.urldecode($_POST['location']));
-    } else {
+    //if (isset($_POST['location'])) {
+     //   header('Location: http://'.$_SERVER['HTTP_HOST'].'/'.$rel_web_path.'/'.urldecode($_POST['location']));
+    //} else {
         // if no location is specified go to list view.
-        header('Location: http://'.$_SERVER['HTTP_HOST'].'/'.$rel_web_path.'/'.'listview.php');
-    }
+     //   header('Location: http://'.$_SERVER['HTTP_HOST'].'/'.$rel_web_path.'/'.'listview.php');
+    //}
 }
 ?>
