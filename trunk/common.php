c46fd0058c5c8b65db43b7a7821c09b9ea293921
toby.anderson <toby.anderson@aa218b52-6c23-11de-927a-590d86faf14d>
2010-09-20 06:04:52 +0000

    Added a favicon.

--- trunk/common.php
+++ trunk/common.php
@@ -23,9 +23,10 @@ function headA($scripts)
 <!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\"
     \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">
 <html xmlns=\"http://www.w3.org/1999/xhtml\">
-<head>
+<head profile=\"http://www.w3.org/2005/10/profile\">
     <meta http-equiv=\"Content-Type\" content=\"text/html;charset=utf-8\" />
     <link rel=\"stylesheet\" type=\"text/css\" href=\"css/filingcabinet-default.css\" />
+    <link rel=\"icon\" type=\"image/png\" href=\"images/favicon.png\" />
 ";
     foreach ($scripts as $script => $type)
     {
