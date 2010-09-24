e01eecc628604573d318503883577fc51929f45c
toby.anderson <toby.anderson@aa218b52-6c23-11de-927a-590d86faf14d>
2010-09-24 02:47:25 +0000

    Found typo in add_to_cabinet code and fixed.

--- listview.php
+++ listview.php
@@ -178,13 +178,11 @@ echo headA(array("js/jquery-1.4.2.min.js" => "text/javascript", "js/jquery-ui-1.
         }); 
         
         $('#add_to_cabinet').click(function() {
-            $('#file-uploaded .qq-uploader .qq-upload-list tr').each(function(index){
+            $('#file-uploader .qq-uploader .qq-upload-list').children().each(function(index){
                 if ($(this).hasClass('qq-upload-fail')) {
-                   $('#info').append(index + ' file failed <br />');
                    // delete rows where the upload failed
                    $(this).remove();
                 } else if ($(this).hasClass('qq-upload-success')) {
-                   $('#info').append(index + ' file succeeded <br />');
                 }
                 // if it has neither the fail nor success class then it hasn't finished uploading
                 // in this case just leave it.
