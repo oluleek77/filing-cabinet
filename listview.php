4e058113ef520603c4d981f9bdcefaf2c27c2d9f
toby.anderson <toby.anderson@aa218b52-6c23-11de-927a-590d86faf14d>
2010-10-05 11:01:38 +0000

    Took out the old upload form. Also changed doctype to xhtml-transitional, and took out the validation button. The new uploader uses iframes which don't validate in strict xhtml (though the w3c validation tool only seems to validate static content, not dynamic, so it's hard to tell whether my app conforms to anything.)

--- listview.php
+++ listview.php
@@ -138,7 +138,7 @@ echo headA(array("js/jquery-1.4.2.min.js" => "text/javascript", "js/jquery-ui-1.
             }
         });
         
-        $('.button').button();
+        //$('.button').button();
         
         $("#login_title").click(function(){
             $('#login_content').toggle(400);
@@ -331,39 +331,6 @@ if ($_GET['action'] == 'delete')
 <div class="main main_toggle_panel" id="upload_panel">
   <div class="toggle_panel_top" id="upload_title">Upload Files</div>
   <div id="upload_content" class="begin_hidden_js">
-    <form enctype="multipart/form-data" action="uploader.php" method="post">
-      <!-- <input type="hidden" name="MAX_FILE_SIZE" value="10485760" /> -->
-      <table id='upload'>
-        <tr>
-          <th title='Choose a file to upload'>Files to upload</th>
-          <th title='Rename the file (optional)'>Edit filename</th>
-          <th title='Comma separated list of labels for the file'>Labels</th>
-          <th title='Allow anyone to download the file?'>Public</th>
-          <th title='File follows in sequence from previous file?'>Sequence</th>
-        </tr>
-<?php
-// amount of files that can be uploaded in one go
-$amount = 5;
-for ($i = 1; $i <= $amount; ++$i):
-?>
-  <tr>
-    <td><input title='Choose a file to upload' name='uploadedfile_<?php echo $i ?>' type='file' onchange="document.getElementById('name_<?php echo $i ?>').value = this.value" /></td>
-    <td><input title='Rename the file (optional)' name='filename_<?php echo $i ?>' id='name_<?php echo $i ?>' type='text' /></td>
-    <td><input title='Comma separated list of labels for the file' name='labels_<?php echo $i ?>' type='text' /></td>
-    <td><input title='Allow anyone to download the file?' name='public_<?php echo $i ?>' type='checkbox' /></td>
-    <?php if ($i > 1): // don't need sequence option on the first file ?> 
-        <td><input title='File follows in sequence from previous file?' name='sequence_<?php echo $i ?>' type='checkbox' /></td>
-    <?php endif; ?>
-  </tr>
-<?php
-endfor;
-?>
-</table>
-  <div>
-    <input type='hidden' name='amount' value='<?php echo $amount ?>' />
-    <input type='submit' value='Upload Files' />
-  </div>
-</form>
     <div id="file-uploader">       
       <noscript>          
         <p>Please enable JavaScript to use file uploader.</p>
@@ -601,9 +568,9 @@ echo "</p>\n";
 
 </div>
 <div id="foot">
-    <a href="http://validator.w3.org/check?uri=referer"><img
+ <!--   <a href="http://validator.w3.org/check?uri=referer"><img
         src="http://www.w3.org/Icons/valid-xhtml10-blue.png"
-        alt="Valid XHTML 1.0 Strict" height="31" width="88" /></a>
+        alt="Valid XHTML 1.0 Strict" height="31" width="88" /></a> -->
 </div>
 
 <?php
